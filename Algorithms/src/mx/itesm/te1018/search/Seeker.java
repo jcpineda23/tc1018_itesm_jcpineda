0432e0f3bd6a4efd96d6c46563648077c0e8ad1d
Juan C. Pineda <jcpineda@outlook.com>
2019-06-15 19:52:26 -0500

    Linear Search Commit

---
+++ Algorithms/src/mx/itesm/te1018/search/Seeker.java
@@ -0,0 +1,23 @@
+package mx.itesm.te1018.search;
+
+public class Seeker {
+
+	public static void linearSearch(int [] arr, int value){
+		int index = 0;
+		while(index < arr.length & arr[index] != value){
+			index++;
+		}
+		if(arr[index] == value)
+			System.out.println("Found it at " + index);
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] myArr = {4, 3, 5, 7, 1, 12, 16, 23};
+		linearSearch(myArr, 23);
+	}
+
+}
