8d00abc18054e5b6fa9d5a4848c33f2bc61dedeb
jupined <jupined@JUPINED-LAP.oradev.oraclecorp.com>
2019-08-27 09:34:17 -0500

    First version

--- Algorithms/src/mx/itesm/te1018/search/BinarySearch.java
+++ Algorithms/src/mx/itesm/te1018/search/BinarySearch.java
@@ -2,4 +2,44 @@ package mx.itesm.te1018.search;
 
 public class BinarySearch {
 
+	public int linearSearch(int[] arr, int n) {
+		int i = -1;
+		for(i = 0; i < arr.length; i++) {
+			if(n == arr[i])
+				break;
+			
+		}
+			
+		return i;
+	}
+	
+	public static int binarySearch(int[] arr,int start, int end, int x ) {
+		
+		int middle = (int) Math.floor((end + start)/2);
+		
+		if(arr[middle] == x)
+			return middle;
+		else {
+			if(arr[middle] > x) { //Going left
+				///MAGIC FUNCTION
+				binarySearch(arr, 0, middle, x);
+			}
+			else if(arr[middle] < x) //Go right
+			{
+				///MAGIC FUNCTION
+				binarySearch(arr, middle +1, end, x);
+			}
+		}
+		return -1;
+	}
+	
+	public static void main (String[] args) {
+		
+		int[] myArray = {1, 2, 4, 6, 8,10, 12, 14};
+		
+		System.out.println("Found at: " + 
+						binarySearch(myArray, 0, myArray.length, 4));
+		
+	}
+	
 }
