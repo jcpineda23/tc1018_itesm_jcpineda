868ec426e413a8b1d419b2e65d663d24d7eee50c
jupined <jupined@JUPINED-LAP.oradev.oraclecorp.com>
2019-10-08 08:17:56 -0500

    Changes to test

--- Algorithms/src/mx/itesm/tc1018/trees/BinaryTreeTest.java
+++ Algorithms/src/mx/itesm/tc1018/trees/BinaryTreeTest.java
@@ -6,12 +6,15 @@ public class BinaryTreeTest {
 		// TODO Auto-generated method stub
 		
 		BinaryTreeNode tmpValue = new BinaryTreeNode(12);
+		BinaryTreeNode anotherValue = new BinaryTreeNode(7);
 		BinaryTreeNode root = null;
 		
 		BinaryTree bt = new BinaryTree();
 		root = bt.insertNode(root, tmpValue);
+		bt.insertNode(root, anotherValue);
 		
-		
+		System.out.println(root.value);
+		System.out.println(root.left.value);
 
 		
 		
